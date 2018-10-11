7a5e7b0bfb7ba89157a863e85b4f6411a39972c4
maaya00 <29718735+maaya00@users.noreply.github.com>
2018-10-11 14:34:53 -0400

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,16 @@
+#lab1
+a='art vandelay'
+b='ceo'
+c='art.vandelay@vandelay.co'
+d='vandelay.com'
+e='7285553334'
+
+f=a.title()
+g=b.upper()
+h=c.endswith('.com')
+i=d.endswith('.com')
+e=int(e)
+j = (e+1)
+l = (e+2)
+print (f, g, h, i, j, l)
+
